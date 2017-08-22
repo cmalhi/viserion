77ab3d0d0cc4392d3db9856cb1a18327a5a9953a
Ed Sweezey <esweezey@yahoo.com>
2017-08-22 19:38:24 -0400

    Update ChooseColor.js

--- ChooseColor.js
+++ ChooseColor.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Alert, AppRegistry, Button, StyleSheet, Text, View } from 'react-native';
 
-export default class App extends React.Component {
+export default class ChooseColor extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
