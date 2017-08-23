01ece8586368245ad6d0a82d82ec68f32774b0c7
Ed Sweezey <esweezey@yahoo.com>
2017-08-23 17:21:56 -0400

    Update titleReducer.js

--- js/reducers/titleReducer.js
+++ js/reducers/titleReducer.js
@@ -1,7 +1,7 @@
  export default function (state = 'default title', action) {
   switch (action.type) {
     case 'ADD_TITLE':
-      return action.payload.data
+      return action.payload
     default: 
       return state;
   }
