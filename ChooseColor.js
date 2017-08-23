39479b283a1c807be31928315a490421c1780909
Chetanpreet Malhi <chemalhi@gmail.com>
2017-08-22 20:28:41 -0400

    Added ChooseTitle.js File

--- ChooseColor.js
+++ ChooseColor.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import { Alert, AppRegistry, Button, StyleSheet, Text, View } from 'react-native';
 
-export default class App extends React.Component {
+export default class ChooseColor extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       color: ''
     }
     this.buttonPress = this.buttonPress.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   buttonPress(color) {
+    //Animate button so the user knows what theyre about to submit
     console.log('You pressed the', color, 'button');
     this.setState({color: color});
+  }
+
+  handleSubmit() {
+    //Send data to DB
+    console.log('Handle submit function. Color: ', this.state.color);
   }
 
   render() {
@@ -28,6 +35,11 @@ export default class App extends React.Component {
             onPress={this.buttonPress.bind(this, 'blue')}
             title="Blue"
             color="#0000FF"
+          />
+          <Button
+            onPress={this.handleSubmit}
+            title="Submit"
+            color="#000000"
           />
       </View>
     );
