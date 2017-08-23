560a988dc8cfd150e990ede092cc9cc302ec24b9
Adrienne Tran <adrienne@adriennetran.com>
2017-08-23 19:21:27 -0400

    Commented routes

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -4,12 +4,20 @@ const router = express.Router();
 const Page = require('../models/page');
 const User = require('../models/user');
 
+/*
+ * /POST /preferences
+ * Receives a JSON of type { layout: [], color: [], title: '' }
+ */
 router.post('/preferences', function(req, res) {
   var newUser = new User({ preferences: req.body });
   newUser.save(function(err) {
     if (err) return console.err('Err saving user: ', err);
     res.status(200).send('Posted!')
   });
+});
+
+router.post('/generate', function(req, res) {
+
 });
 
 module.exports = router;
