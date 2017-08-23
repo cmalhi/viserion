b5f86541a6798b5731a0f1ce46cd4d512916134a
Adrienne Tran <adrienne@adriennetran.com>
2017-08-23 14:46:09 -0400

    Added /POST route to preferences

--- app/models/user.js
+++ app/models/user.js
@@ -6,7 +6,7 @@ const userSchema = mongoose.Schema({
   password: String,
   email: String,
   name: String,
-  Preferences: { Layout: [String], Color: [String], Title: String },
+  preferences: { layout: [String], color: [String], title: String },
 });
 
 userSchema.plugin(findOrCreate);
--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const db = require('../../config/database');
 const router = express.Router();
-const Page = require('./models/page');
-const User = require('./models/user');
+const Page = require('../models/page');
+const User = require('../models/user');
 
 router.post('/preferences', function(req, res) {
-  const preferences = req.data;
-  console.log('req.data');
-  // update db with layout preference
-  // var newUser = new User({ preferences: req.data });
-  res.send('Posted!')
+  var newUser = new User({ preferences: req.body });
+  newUser.save(function(err) {
+    if (err) return console.err('Err saving user: ', err);
+    res.status(200).send('Posted!')
+  });
 });
 
 module.exports = router;
