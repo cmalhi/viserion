65045d06511d9f1d59f32a7de5f950b4d11f2262
Chetanpreet Malhi <chemalhi@gmail.com>
2017-08-22 20:49:32 -0400

    Added ChooseTitle to the navigation menu

--- js/App.js
+++ js/App.js
@@ -2,22 +2,13 @@ import React from 'react';
 import { StyleSheet, Button, Text, View, ScrollView } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 import ChooseColor from './ChooseColor';
+import ChooseTitle from './ChooseTitle'
 
 export default () => <AppNavigator />;
 
 class TemplateScreen extends React.Component {
   render() {
     return <Text>Choose a template!</Text>
-  }
-}
-
-class ColorScreen extends React.Component {
-  render() {
-    return (
-      <View>
-        <Text>Choose a color!</Text>
-      </View>
-    )
   }
 }
 
@@ -30,6 +21,7 @@ class HomeScreen extends React.Component {
       <ScrollView>
         <Button onPress={() => { navigate('Color')}} title="Color" />
         <Button onPress={() => { navigate('Template')}} title="Template" />
+        <Button onPress={() => { navigate('Title')}} title="Title" />
       </ScrollView>
     )
   }
@@ -38,7 +30,8 @@ class HomeScreen extends React.Component {
 const AppNavigator = StackNavigator({
   Index: { screen: HomeScreen },
   Color: { screen: ChooseColor },
-  Template: { screen: TemplateScreen }
+  Template: { screen: TemplateScreen },
+  Title: { screen: ChooseTitle }
 });
 
 const styles = StyleSheet.create({
