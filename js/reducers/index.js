cee96cab1a83f817467089891144b5d206941f56
Ed Sweezey <esweezey@yahoo.com>
2017-08-24 19:18:36 -0400

    Update index.js

--- js/reducers/index.js
+++ js/reducers/index.js
@@ -2,13 +2,11 @@ import { combineReducers } from "redux";
 import layoutReducer from "./layoutReducer";
 import colorReducer from "./colorReducer";
 import titleReducer from "./titleReducer";
-import { reducer as formReducer } from 'redux-form';
 
 const rootReducer = combineReducers({
   layouts: layoutReducer,
   colors: colorReducer,
   title: titleReducer,
-  form: formReducer,
 });
 
 export default rootReducer;
