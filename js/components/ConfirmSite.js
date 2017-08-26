9f4c87e4bef5cfaecce45f590100602f60c8197b
Ed Sweezey <esweezey@yahoo.com>
2017-08-25 20:24:12 -0400

    just about complete with confirm site

--- js/components/ConfirmSite.js
+++ js/components/ConfirmSite.js
@@ -2,79 +2,61 @@ import React from 'react'
 import {
   Text,
   View,
-  WebView
+  WebView,
+  Button
 } from 'react-native'
 import Swiper from 'react-native-swiper'
 
-var styles = {
-  wrapper: {
-  },
-  slide1: {
+const styles = {
+  wrapper: {},
+  slides: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#22965A'
-  },
-  slide2: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#910F66'
-  },
-  slide3: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#92BBD9'
-  },
-  slide4: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F4BBD4'
-  },
-  text: {
-    color: '#fff',
-    fontSize: 30,
-    fontWeight: 'bold'
+    backgroundColor: 'rgba(0,0,0,0)'
   }
 }
 
 var source =  [{uri: 'http://google.com'},{uri: 'http://spotify.com'},{uri: 'http://nfl.com'},{uri: 'http://cnn.com'}];
 
-export default class ConfirmSite extends React.Component {
+class ConfirmSite extends React.Component {
+  constructor(props){
+      super(props);
+      this.state = {
+        uris: []
+      }
+      this.handlePress = this.handlePress.bind(this);
+  }
+
+  componentDidMount(){
+    this.setState({uris: [...this.state.uris, ...source]},console.log('source',...source));
+  }
+
+  handlePress(index){
+   
+    console.log('The URI clicked was',this.state.uris[index].uri);
+  }
+
   render() {
+    var slides = [];
+    for(var u = 0; u < this.state.uris.length; u +=1) {
+      slides.push(<View key={u} style={styles.slides}>
+                    <WebView style={{padding: 20, width:400}}
+                    automaticallyAdjustContentInsets={false}
+                    scrollEnabled={true}
+                    source={this.state.uris[u]}>
+                    </WebView>
+                    <Button  title="Submit" onPress={this.handlePress.bind(this, u)} />
+                  </View>
+      )
+
+    }
     return (
       <Swiper style={styles.wrapper} showsButtons>
-        <View style={styles.slide1}>
-          <WebView style={{padding: 20, width:400}}
-          automaticallyAdjustContentInsets={false}
-          scrollEnabled={true}
-          source={source[0]}>
-         </WebView>
-        </View>
-        <View style={styles.slide2}>
-          <WebView style={{padding: 20, width:400}}
-          automaticallyAdjustContentInsets={false}
-          scrollEnabled={true}
-          source={source[1]}>
-          </WebView>
-        </View>
-        <View style={styles.slide3}>
-          <WebView style={{padding: 20, width:400}}
-          automaticallyAdjustContentInsets={false}
-          scrollEnabled={false}
-          source={source[2]}>
-          </WebView>
-        </View>
-        <View style={styles.slide4}>
-         <WebView style={{padding: 20, width:400}}
-          automaticallyAdjustContentInsets={false}
-          scrollEnabled={false}
-          source={source[3]}>
-          </WebView>
-        </View>
+        {slides} 
       </Swiper>
     )
   }
 }
+
+export default ConfirmSite;
