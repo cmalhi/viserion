7e68dc07add5465d6829b8afc4ba8c0c78ee06d6
Ed Sweezey <esweezey@yahoo.com>
2017-08-26 11:01:34 -0400

    changed database to mlab and added confirm site over to redux

--- js/reducers/index.js
+++ js/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import layoutReducer from "./layoutReducer";
 import colorReducer from "./colorReducer";
 import titleReducer from "./titleReducer";
+import siteReducer from "./siteReducer";
 
 const rootReducer = combineReducers({
   layouts: layoutReducer,
   colors: colorReducer,
   title: titleReducer,
+  site: siteReducer,
 });
 
 export default rootReducer;
