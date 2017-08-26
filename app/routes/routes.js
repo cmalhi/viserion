02c97c7b872b3977bbb76fbca86ffd46564a3439
Matthew Go <matthewaarongo@gmail.com>
2017-08-25 20:02:49 -0400

    Template retrieval, combination, and user preference integration

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -47,31 +47,96 @@ router.post('/preferences', function(req, res) {
  */
 router.post('/generate', function(req, res) {
   // Get user preferences
-  const userPreferences = { layout: ['standard'], color: ['blue'], title: "Chetan's Milk Shop"};
+  const userPreferences = { layout: ['standard'], colors: ['blue', 'green'], title: "Chetan's Milk Shop"};
 
   const beg = '<!DOCTYPE html><html lang="en">';
   const end = '</body></html>';
 
   const fileNames = ['head.html', 'style.html', 'hero.html', 'content.html', 'footer.html'];
 
-  // Finds file names in file table and concatenates bodies of each file object
-  Promise.all(fileNames.map(file => File.find({name: file}).exec()))
-    .then(files => {
-      const page = files.reduce((acc, item) => {
-        return acc + item[0].body
-      }, '');
-      const customPage = page.replace('${BG-COLOR}', userPreferences.color).replace('${TITLE}', userPreferences.title);
-      const finalPage = beg + customPage + end;
+  // Create templates for each combination or user selected style
 
-      // TODO: update user id
-      UserTemplate.create({body: finalPage, userid: '1'})
-        .then(template => {
-          console.log(template);
-          res.send(finalPage);
+  // Replace color and title for each template
+
+  // Finds file names in file table and concatenates bodies of each file object
+  var fileComponents = {};
+  var query = { keywords: ['basic'] };
+
+  File.find(query).exec()
+    .then(files => {
+      // console.log('ALL FILES: ',files);
+      files.map(file => {
+        const section = file.section;
+        if (fileComponents[section]) {
+          fileComponents[section].push(file);
+        } else {
+          fileComponents[section] = [file];
+        }
+        // fileComponents[section] = fileComponents[section] ? fileComponents[section].push(file) : [file]
+      })
+
+      // Interate through fileComponenets 
+      // Produce combinations of components
+      var produceCombinations = (obj) => {
+        var keys = Object.keys(obj);
+        var combinations = [];
+        // console.log(' file obj >>', obj);
+        function recur(currCombination, i) {
+          // base case
+          if (i === keys.length) return combinations.push(currCombination)
+          
+          // recursive case
+          for (var inner = 0; inner < obj[keys[i]].length; inner++) {
+            recur(currCombination + obj[keys[i]][inner].body, i+1)
+          }
+        }
+        recur('', 0);
+        return combinations;
+      }
+
+      const combinations = produceCombinations(fileComponents);
+      // let customPages = [];
+      // Replace with user preferences
+        // Handle colors
+        // Insert title
+      async.each(combinations, function(combination) {
+        userPreferences.colors.forEach((color) => {
+          let page = combination.replace('${BG-COLOR}', color).replace('${TITLE}', userPreferences.title);
+          console.log('newPage', page);
+          page = beg + page + end;
+          // customPages.push(beg + page + end);
+          // Store combinations in DB
+          UserTemplate.create({body: page, userid: '1'})
+            .then(template => {
+              console.log(template);
+            })
+            .catch(err => console.log(err));
         })
-        .catch(err => console.log(err));
+        // console.log('customPages', customPages)
+        // cb();
+        res.send('User pages generated');
+      })
+      
+
+      // combinations.forEach((combination) => { 
+        
+      // })
+
+      
+
+      
+
+
 
     });
+
+      // const page = files.reduce((acc, item) => {
+      //   return acc + item[0].body
+      // }, '');
+
+      // TODO: update user id
+
+
 });
 
 module.exports = router;
