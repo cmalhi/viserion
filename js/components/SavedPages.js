1c4057988f9f7b587a16e6cd270d70275640e600
Adrienne Tran <adrienne@adriennetran.com>
2017-08-26 13:34:11 -0400

    Added screenshot to usertemplate code

---
+++ js/components/SavedPages.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+export default class SavedPages extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    // TODO: Get templates from userTemplates
+  }
+
+  render() {
+    return (
+      <Text>Hello!</Text>
+    )
+  }
+}
