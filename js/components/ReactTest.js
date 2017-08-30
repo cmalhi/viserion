7e6569e9bf140107eec13b96bf159de769759182
Adrienne Tran <adrienne@adriennetran.com>
2017-08-30 18:51:52 -0400

    react component

--- js/components/ReactTest.js
+++ js/components/ReactTest.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Text, View, WebView, Button } from 'react-native';
 
-export default class PageScreen extends React.Component {
+export default class ReactTest extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
