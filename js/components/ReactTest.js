e2ce492857bc1e80f5a3930900621cab93c56870
Chetanpreet Malhi <chemalhi@gmail.com>
2017-08-31 17:03:47 -0400

    react client set up to be scalable to add components

--- js/components/ReactTest.js
+++ js/components/ReactTest.js
@@ -9,9 +9,11 @@ export default class ReactTest extends React.Component {
     this.state = {
       html: html,
       js: js,
+      prefs: userPrefrences,
     };
   };
   componentDidMount() {
+    socket.on('add', {})
   }
   render() {
     return (
@@ -21,6 +23,10 @@ export default class ReactTest extends React.Component {
       />
     )
   };
+}
+
+var userPrefrences = {
+  components: ["React.createElement(MyComponent, null)", "React.createElement(MyComponent2, null)"]
 }
 
 const html = `
@@ -34,16 +40,72 @@ container
 `;
 
 const js = `
+  var MyComponent2 = React.createClass({
+    displayName: 'MyComponent2',
+  
+    render: function render() {
+      return React.createElement(
+        'div',
+        { fontSize: 60, fontWeight: 'bold' },
+        'More lines'
+      );
+    }
+  });
   var MyComponent = React.createClass({
     displayName: 'MyComponent',
   
     render: function render() {
       return React.createElement(
         'div',
-        { fontSize: 70, fontWeight: 'bold' },
-        'Hello'
+        { fontSize: 60, fontWeight: 'bold' },
+        'A line'
       );
     }
   });
-  ReactDOM.render(React.createElement(MyComponent, null), document.getElementById('container'));
+  var compArr = [React.createElement(MyComponent, null), React.createElement(MyComponent2, null)];
+  var components = React.createElement(
+    "div",
+    null, 
+    ...compArr
+  );
+  ReactDOM.render(components, document.getElementById('container'));
 `;
+
+  //the components array will be pre sorted from the "add components" page
+
+
+//   const html = `
+// <script src="https://cdnjs.cloudflare.com/ajax/libs/react/15.0.1/react.js"></script>
+// <script src="https://cdnjs.cloudflare.com/ajax/libs/react/15.0.1/react-dom.js"></script>
+// <body>
+// <div id="container">
+// container
+// </div>
+// </body>
+// `;
+
+// const js = `
+//   var MyComponent = React.createClass({
+//     displayName: 'MyComponent',
+  
+//     render: function render() {
+//       return React.createElement(
+//         'div',
+//         { fontSize: '90', fontWeight: 'bold' },
+//         'Hello i am the first div that should render on this page'
+//       );
+//     }
+//   });
+  // var MyComponent2 = React.createClass({
+  // displayName: 'MyComponent2',
+
+  // render: function render() {
+  //   return React.createElement(
+  //     'div',
+  //     { fontSize: 80, fontWeight: 'bold' },
+  //     'Another line'
+  //   );
+  // }
+// });
+//   ReactDOM.render(React.createElement(MyComponent2, null), document.getElementById('container'));
+// `;
