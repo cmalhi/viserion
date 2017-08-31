98ff1692b8a40bfb6c2c9b0e871e2a9596e591cf
Adrienne Tran <adrienne@adriennetran.com>
2017-08-30 22:28:38 -0400

    sockets working

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const routes = require('./app/routes/routes');
 
 const app = express();
+const server = app.listen(process.env.PORT || 8080, () => console.log(`Listening on ${process.env.PORT}`));
+const io = require('socket.io').listen(server);
+const routes = require('./app/routes/routes')(io);
 
 // Middleware
 app.use(bodyParser.json());
@@ -18,6 +20,6 @@ app.get('/', (req, res) => {
   res.send('Connected!');
 });
 
-app.listen(process.env.PORT || 8080, function() {
-  console.log('Express server is up and running!');
-});
+// app.listen(process.env.PORT || 8080, function() {
+//   console.log('Express server is up and running!');
+// });
