f52917a5b85020aee4deb122dc8949d1b73dbedb
Adrienne Tran <adrienne@adriennetran.com>
2017-08-31 19:57:33 -0400

    Merge dfrom dev

--- global.js
+++ global.js
@@ -1,3 +1,12 @@
 global.HOST = 'http://127.0.0.1:8080';
 // global.HOST = 'http://ec2-54-203-8-222.us-west-2.compute.amazonaws.com:8080';
 // global.HOST = 'http://ac33a321.ngrok.io'; // Ed's
+
+global.AWSEC3 = {
+  keyPrefix: "uploads/",
+  bucket: "viserion-hr",
+  region: "us-east-1",
+  accessKey: "AKIAI4U73XJ3DJ4PGJYQ",
+  secretKey: "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",
+  successActionStatus: 201
+};
