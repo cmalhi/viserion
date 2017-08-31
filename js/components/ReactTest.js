63632775512b034cf7246aaff9d3413d04f146ea
Chetanpreet Malhi <chemalhi@gmail.com>
2017-08-31 17:44:50 -0400

    setting up skeleton for user order choice

--- js/components/ReactTest.js
+++ js/components/ReactTest.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, WebView, Button } from 'react-native';
+import { Text, View, WebView, Button, TextInput } from 'react-native';
 const io = require('socket.io-client');
 const socket = io('http://127.0.0.1:8080', { transports: ['websocket'] });
 
@@ -10,17 +10,62 @@ export default class ReactTest extends React.Component {
       html: html,
       js: js,
       prefs: userPrefrences,
+      order: '',
     };
+    this.handlePress = this.handlePress.bind(this);
+    this.listeners = this.listeners.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   };
   componentDidMount() {
-    socket.on('add', {})
   }
+
+  listeners() {
+  }
+  
+  handlePress() {
+  }
+  handleSubmit() {
+    var orderStr = this.state.order;
+    var orderArr = orderStr.split('');
+    console.log('This is the order string ',orderArr)
+    var result = [];
+    // map the components array here
+    for (var i = 0; i < orderArr.length; i++) {
+    //use switch when scaling this instead of chaining if statements
+    //if element is 1 then push my component 1
+      if (orderArr[i] === '1') {result.push('React.createElement(MyComponent, null)');}
+      if (orderArr[i] === '2') {result.push('React.createElement(MyComponent2, null)');}
+      console.log('the current result being pushed is ', result[i])
+    }
+    //if element is 2 then push my component 2
+    //send the array to react client
+    //react client updates state to re-render components
+  }
+  
   render() {
     return (
-      <WebView
-        source={{html: this.state.html}}
-        injectedJavaScript={this.state.js}
-      />
+      <View style={styles.slides}>
+        <WebView style={{padding: 10, width:320 }}
+          ref='webview'
+          automaticallyAdjustContentInsets={false}
+          scrollEnabled={true}
+          scalesPageToFit={true}
+          source={{html: this.state.html}}
+          javaScriptEnabled={true}
+          injectedJavaScript={this.state.js}
+          javaScriptEnabledAndroid={true}   
+          >
+        </WebView>
+        <TextInput
+          style={{height: 40}}
+          placeholder="pick order"
+          onChangeText={(order) => this.setState({order}, ()=> {console.log('order is now: ', this.state.order)})}
+          onSubmitEditing={this.handleSubmit}
+          clearButtonMode={'unless-editing'}
+          keyboardType={"default"}
+        />
+        <Button title={'Submit'} onPress={this.handleSubmit} />
+      </View>
     )
   };
 }
@@ -70,6 +115,16 @@ const js = `
   );
   ReactDOM.render(components, document.getElementById('container'));
 `;
+
+const styles = {
+  wrapper: {},
+  slides: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0,0,0,0)',
+  }
+}
 
   //the components array will be pre sorted from the "add components" page
 
