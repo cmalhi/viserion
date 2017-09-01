8bd1a15f1333182002f82a5a23ae9443d34a9cdd
Chetanpreet Malhi <chemalhi@gmail.com>
2017-09-01 16:21:55 -0400

    removed screenshot (chetans comp has a bug)

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const Promise = require('bluebird');
 const fs = require('fs');
 Promise.promisifyAll(fs);
-const Screenshot = require('url-to-screenshot');
+// const Screenshot = require('url-to-screenshot');
 const User = require('../models/user');
 const File = require('../models/file');
 const UserTemplate = require('../models/userTemplate');
@@ -51,21 +51,21 @@ var routerInstance = function(io) {
     console.log('submitchoice');
     const templateId = "59a19409bc1b89b728fe07cb";
 
-    new Screenshot('/usertemplates/' + templateId)
-      .width(1080)
-      .height(1920)
-      .clip()
-      .capture()
-      .then(img => {
-        // TODO: Store in S3
+    // new Screenshot('/usertemplates/' + templateId)
+    //   .width(1080)
+    //   .height(1920)
+    //   .clip()
+    //   .capture()
+    //   .then(img => {
+    //     // TODO: Store in S3
 
-        const screenshotUrl = __dirname + '/../../js/components/example.png';
-        fs.writeFileSync(screenshotUrl, img);
-        console.log(screenshotUrl);
+    //     const screenshotUrl = __dirname + '/../../js/components/example.png';
+    //     fs.writeFileSync(screenshotUrl, img);
+    //     console.log(screenshotUrl);
 
-        userTemplateController.upsert({_id: templateId}, {screenshot: screenshotUrl})
-          .then(updatedDoc => console.log('new screenshotUrl: ' + updatedDoc.screenshot));
-      });
+    //     userTemplateController.upsert({_id: templateId}, {screenshot: screenshotUrl})
+    //       .then(updatedDoc => console.log('new screenshotUrl: ' + updatedDoc.screenshot));
+    //   });
   });
 
   /*
