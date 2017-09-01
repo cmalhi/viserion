01c5d9bb0b1c47970d45eecb12e246472175f80c
Adrienne Tran <adrienne@adriennetran.com>
2017-09-01 15:23:08 -0400

    Adjusted un-used code

--- app/sockets.js
+++ app/sockets.js
@@ -2,6 +2,7 @@
 var socketInstance = function(io) {
   io.on('connection', function(socket) {
 
+    // Title
     socket.on('titleChange', function(title) {
       io.sockets.emit('titleChange', title);
       // TODO: .to(x) to SPECIFY SOCKET ID;
@@ -11,12 +12,18 @@ var socketInstance = function(io) {
       io.sockets.emit('titleChange2', title);
     });
 
+    // Image
     socket.on('imgChange', function(img) {
       io.sockets.emit('imgChange', 'ok changing');
     });
 
     socket.on('imgChange2', function(img) {
       io.sockets.emit('imgChange2', img);
+    });
+
+    // Header color
+    socket.on('colorChange', function(data) {
+      io.sockets.emit('colorChange', data);
     });
 
   });
