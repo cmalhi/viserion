1f2bf9047f6f492fe9b3c2ab4eddbc3708a5f2cb
Ed Sweezey <esweezey@yahoo.com>
2017-09-01 18:11:44 -0400

    fixing merge changes

--- global.js
+++ global.js
@@ -1,6 +1,6 @@
 import { AWSEC3 } from './config/config';
 
-global.HOST = 'http://127.0.0.1:8080';
+//global.HOST = 'http://127.0.0.1:8080';
 // global.HOST = 'http://ec2-54-203-8-222.us-west-2.compute.amazonaws.com:8080';
 // global.HOST = 'http://ac33a321.ngrok.io'; // Ed's
 
--- js/components/App.js
+++ js/components/App.js
@@ -64,7 +64,6 @@ class HomeScreen extends React.Component {
           ping response: {this.state.data}
         </Text>}
         <Text style={styles.title}>Create a page</Text>
-<<<<<<< HEAD
         <Text onPress={() => { navigate('SignUp')}} style={styles.defaultText}>Step 0: Sign Up</Text>
         <Text onPress={() => { navigate('Login')}} style={styles.defaultText}>Step 0.5: Log In</Text>
         <Text onPress={() => { navigate('Template')}} style={styles.defaultText}>Step 1: Template</Text>
@@ -75,23 +74,10 @@ class HomeScreen extends React.Component {
         <Text onPress={() => { navigate('ShareScreen')}} style={styles.defaultText}>Step 6: Share Link </Text>
         <Text onPress={() => { navigate('Page')}} style={styles.defaultText}>Final Page</Text>
         <Text onPress={() => { navigate('Saved')}} style={styles.inDevelopment}>In development: Saved Pages</Text>
-        <Text onPress={() => { navigate('AddPageComponent')}} style={styles.inDevelopment}>In development: New Component</Text>
         <Text onPress={() => { navigate('UserEditTest')}} style={styles.inDevelopment}>In development: User Editing</Text>
-=======
-        <Text onPress={() => { navigate('SignUp')}} style={styles.defaultText,styles.selectedText}>Step 0: Sign Up</Text>
-        <Text onPress={() => { navigate('Login')}} style={styles.defaultText,styles.selectedText}>Step 0.5: Log In</Text>
-        <Text onPress={() => { navigate('Template')}} style={styles.defaultText,styles.selectedText}>Step 1: Template</Text>
-        <Text onPress={() => { navigate('Color')}} style={styles.defaultText,styles.selectedText}>Step 2: Color</Text>
-        <Text onPress={() => { navigate('Keywords')}} style={styles.defaultText,styles.selectedText}>Step 3: Keywords</Text>
-        <Text onPress={() => { navigate('Title')}} style={styles.defaultText,styles.selectedText}>Step 4: Title</Text>
-        <Text onPress={() => { navigate('ConfirmSite')}} style={styles.defaultText,styles.selectedText}>Step 5: Confirm Selection</Text>
-        <Text onPress={() => { navigate('ShareScreen')}} style={styles.defaultText,styles.selectedText}>Step 6: Share Link </Text>
-        <Text onPress={() => { navigate('Page')}} style={styles.defaultText,styles.selectedText}>Final Page</Text>
         <Text onPress={() => { navigate('Image')}} style={styles.defaultText,styles.selectedText}>(Optional) Add Image</Text>
-        <Text onPress={() => { navigate('Saved')}} style={styles.defaultText,styles.selectedText}>Saved Pages</Text>
         <Text onPress={() => { navigate('AddPageComponent')}} style={styles.defaultText}>AddPageComponent Test</Text>
         <Text onPress={() => { navigate('ImageSearch')}} style={styles.defaultText}>Image Search</Text>
->>>>>>> myFeat1
       </View>
     )
   }
@@ -111,11 +97,8 @@ const  AppNavigator = StackNavigator({
   Login: { screen: Login },
   SignUp: { screen: SignUp },
   AddPageComponent: { screen: AddPageComponent },
-<<<<<<< HEAD
   UserEditTest: { screen: UserEdit },
-=======
   ImageSearch: { screen: ImageSearch },
->>>>>>> myFeat1
 });
 
 const styles = StyleSheet.create({
