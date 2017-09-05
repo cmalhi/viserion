aa4ba1feb2906a908c715c764362e982610c2186
Chetanpreet Malhi <chemalhi@gmail.com>
2017-09-05 11:52:55 -0400

    Making the add page component entry modular

--- js/components/modals/AddPageModalEntry.js
+++ js/components/modals/AddPageModalEntry.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import { Text, TouchableHighlight, Image } from 'react-native';
+import { Text, TouchableHighlight, Image, View, StyleSheet } from 'react-native';
 
 export default class AddPageModalEntry extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
   render() {
+    console.log('WE ARE IN THE ADD PAGE COMPONENT')
     return (
-      <Text style={styles.bigText}>Pricing</Text>
-      <Image
-        style={{width: 194, height: 120}}
-        source={require('../../../images/components/pricing.png')}
-      />
+      <View>
+        <Text style={styles.bigText}>Name: {this.props.component.name}</Text>
+       <Image
+         style={{width: 194, height: 120}}
+         source={require(`../../../images/components/${this.props.component.img}`)}
+        />
+      </View>
     );
   }
 }
@@ -17,34 +24,15 @@ const styles = StyleSheet.create({
   flexContainer: {
     flex: 1,
   },
-  center: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    textAlign: 'center',
-  },
-  webView: {
-    padding: 10,
-    width: '100%',
-  },
-  modal: {
-    backgroundColor: 'rgba(0,0,0,.5)',
-    position: 'absolute',
-    top: 0,
-    right: 0,
-    bottom: 0,
-    left: 0,
-    width: '100%',
-    alignItems: 'center',
-  },
-  innerModal: {
-    width: '80%',
-    backgroundColor: '#fff',
-    padding: 10,
-    position: 'relative',
-    top: '5%',
-    borderRadius: 10,
-  },
   bigText: {
     fontSize: 20,
   },
 });
+
+
+//props needed
+
+//the components name
+//this.props.component.name
+//an image of the component
+//../../../images/components/{this.props.component.image}
