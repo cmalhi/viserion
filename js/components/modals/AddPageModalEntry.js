e96585c99454bbb830564fff673d64229821de46
Chetanpreet Malhi <chemalhi@gmail.com>
2017-09-05 13:13:12 -0400

    pictures now show up to match the component names in the add component modal

--- js/components/modals/AddPageModalEntry.js
+++ js/components/modals/AddPageModalEntry.js
@@ -2,18 +2,19 @@ import React from 'react';
 import { Text, TouchableHighlight, Image, View, StyleSheet } from 'react-native';
 
 export default class AddPageModalEntry extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
   }
 
   render() {
+    const currentPic = `../../../images/components/${this.props.component.img}`
     console.log('WE ARE IN THE ADD PAGE COMPONENT')
     return (
       <View>
         <Text style={styles.bigText}>Name: {this.props.component.name}</Text>
-       <Image
-         style={{width: 194, height: 120}}
-         source={require(`../../../images/components/${this.props.component.img}`)}
+        <Image
+          style={{width: 194, height: 120}}
+          source={this.props.component.img}
         />
       </View>
     );
