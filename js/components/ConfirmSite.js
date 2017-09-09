ab4567fd91e78ace261db479fba4cc00fd0554d4
Matthew Go <matthewaarongo@gmail.com>
2017-09-08 22:02:46 -0400

    Dynamic page rendering, refactor

--- js/components/ConfirmSite.js
+++ js/components/ConfirmSite.js
@@ -11,24 +11,6 @@ import { connect } from 'react-redux';
 import { addSite, selectPreferences } from '../actions/index';
 import axios from 'axios';
 import renderHtmlTemplate from '../htmlTemplate';
-const io = require('socket.io-client');
-
-const rawPreferences = `[{
-  name: 'My Hero',
-  componentName: 'Hero',
-  attr: {
-    bgColor: 'green',
-    title: 'defaultTitle',
-  },
-},
-{
-  name: 'My Footer',
-  componentName: 'Footer',
-  attr: {
-    bgColor: '#009AF2',
-    text: 'defaultText',
-  },
-}]`
 
 class ConfirmSite extends React.Component {
   constructor(props){
@@ -37,29 +19,7 @@ class ConfirmSite extends React.Component {
         preferencesAll: this.props.preferencesAll,
       }
       this.handlePress = this.handlePress.bind(this);
-      // this.getURIs = this.getURIs.bind(this);
   }
-
-  componentDidMount(){
-    // const html = renderHtmlTemplate(JSON.stringify(rawPreferences));
-    // const html2 = renderHtmlTemplate(JSON.stringify(this.props.preferencesAll[0]));
-
-  }
-
- // getURIs(){
- //    axios.get(`${global.HOST}/usertemplates/list`)
- //    .then((response) => {
- //      var result = response.data.map(function(val){
- //        var site = `${global.HOST}/${val}`;
- //        return {uri: site}
- //      });
- //      console.log("result", result)
- //      this.setState({uris: [...result]}, console.log('results from getURIs ', this.state.uris))
- //      //cb(source)
- //    }).catch(function(err){
- //      console.log('There was an error(msg):',err);
- //    })  
-  // }
 
   handlePress(index){
     const { navigate } = this.props.navigation; 
@@ -67,27 +27,28 @@ class ConfirmSite extends React.Component {
     navigate('ShareScreen');   
   }
 
+  renderViews() {
+    return this.props.preferencesAll.map((preference, index) => {
+      const html = renderHtmlTemplate(preference);
+      return (
+        <View key={index} style={styles.slides}>
+          <WebView style={{padding: 10, width:350 }}
+            automaticallyAdjustContentInsets={false}
+            scrollEnabled={true}
+            scalesPageToFit={true}
+            source={{html: html}}>
+          </WebView>
+          <Button title={'Submit'} onPress={this.handlePress.bind(this, index)} />
+        </View>
+      )
+    })
+  }
+
   render() {
-    if(this.props.preferencesAll.length){
-      var slides = [];
-      for(var u = 0; u < this.props.preferencesAll.length; u +=1) {
-        const html = renderHtmlTemplate(this.props.preferencesAll[u]);
-        // console.log('html >>>>>>>>', html);
-        slides.push(
-          <View key={u} style={styles.slides}>
-            <WebView style={{padding: 10, width:350 }}
-              automaticallyAdjustContentInsets={false}
-              scrollEnabled={true}
-              scalesPageToFit={true}
-              source={{html: html}}>
-            </WebView>
-            <Button title={'Submit'} onPress={this.handlePress.bind(this, u)} />
-          </View>
-        )
-      }
+    if(!!this.props.preferencesAll){
       return (
         <Swiper style={styles.wrapper} showsButtons>
-          {slides} 
+          {this.renderViews()}
         </Swiper>
       ) 
     } else {
