6fc34baeb0058764dacf0e1999ee6b8260ccf74d
Matthew Go <matthewaarongo@gmail.com>
2017-09-09 12:22:09 -0400

    Default colors

--- js/reducers/colorReducer.js
+++ js/reducers/colorReducer.js
@@ -1,4 +1,6 @@
-export default function (state = [], action) {
+const defaultColors = ['#A6EEDA', '#8565F3'];
+
+export default function (state = defaultColors, action) {
   switch (action.type) {
     case 'ADD_COLORS':
       const color = action.payload;
