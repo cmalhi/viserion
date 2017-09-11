67e316ba6409f2bafb43aaec64394a424d374bdd
Chetanpreet Malhi <chemalhi@gmail.com>
2017-09-11 13:14:04 -0400

    wired up

--- js/componentMap.js
+++ js/componentMap.js
@@ -68,7 +68,7 @@ const componentMap = {
         'https://68.media.tumblr.com/bf224896567a05f502931d1f738cec85/tumblr_oubn37DsUk1s2tva9o1_1280.jpg',
         'https://68.media.tumblr.com/c0f6f53319b37c3dd46158438739a5ae/tumblr_oubn9jascg1s2tva9o1_1280.jpg',
         'https://68.media.tumblr.com/aac121c777631aa1ef756fd4164a004f/tumblr_oubn8iPPLG1s2tva9o1_1280.jpg',
-      ]
+      ],
     },
   },
   ImageCaption: {
