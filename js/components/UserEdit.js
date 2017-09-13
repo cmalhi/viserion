b3e8c8635c070ecdd7529466e8b6a0dce9e9e73d
Chetanpreet Malhi <chemalhi@gmail.com>
2017-09-13 16:02:17 -0400

    working on wiring up color

--- js/components/UserEdit.js
+++ js/components/UserEdit.js
@@ -114,8 +114,8 @@ class UserEdit extends React.Component {
     return (
       <View style={styles.basicContainer}>
         <View style={styles.basicContainer}>
-          {/*<WebView style={styles.screenWidth} source={{uri: `${global.HOST}/webpages/add.html`}} />*/}
-          <WebView style={styles.screenWidth} source={{uri: `${global.HOST}/${this.props.siteId}`}} />
+          {<WebView style={styles.screenWidth} source={{uri: `${global.HOST}/webpages/add.html`}} />}
+          {/*<WebView style={styles.screenWidth} source={{uri: `${global.HOST}/${this.props.siteId}`}} />*/}
           { this.state.shortTextModal ?
             <ShortTextModal
               data={this.state.shortTextData}
