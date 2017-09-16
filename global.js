caf23323f88773312fe775ed7288791bf1a12568
Matthew Go <matthewaarongo@gmail.com>
2017-09-15 20:25:58 -0400

    Search and pref

--- global.js
+++ global.js
@@ -1,9 +1,9 @@
 import { AWSEC3 } from './config/config';
 
-// global.HOST = 'http://127.0.0.1:8080';
+global.HOST = 'http://127.0.0.1:8080';
 // global.HOST = 'http://ec2-54-203-8-222.us-west-2.compute.amazonaws.com:8080';
 // global.HOST= 'http://542a9fa6.ngrok.io'; //ngrok business plan
-global.HOST = 'http://spindleapp.com:8080';
+// global.HOST = 'http://spindleapp.com:8080';
 
 global.AWSEC3 = {
   keyPrefix: 'uploads/',
