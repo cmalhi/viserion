28507297460c53501d6a1d5624396cc4545efae8
Matthew Go <matthewaarongo@gmail.com>
2017-09-22 14:30:40 -0400

    Action type

---
+++ js/actions/actionTypes.js
@@ -0,0 +1,16 @@
+export const LOGIN_REQUEST = 'LOGIN_REQUEST';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAILURE = 'LOGIN_FAILURE';
+export const ADD_LAYOUTS = 'ADD_LAYOUTS';
+export const ADD_TITLE = 'ADD_TITLE';
+export const ADD_COLORS = 'ADD_COLORS';
+export const ADD_KEYWORDS = 'ADD_KEYWORDS';
+export const APPEND_PREFS = 'APPEND_PREFS';
+export const UPDATE_PREFS = 'UPDATE_PREFS';
+export const CREATE_PREFERENCES = 'CREATE_PREFERENCES';
+export const SELECT_PREFERENCES = 'SELECT_PREFERENCES'; 
+export const SAVE_PREFERENCES = 'SAVE_PREFERENCES';
+export const LOAD_PREFERENCES = 'LOAD_PREFERENCES';
+export const SELECT_SITE = 'SELECT_SITE';
+export const EDIT_SITE = 'EDIT_SITE';
+export const REMOVE_SITE = 'REMOVE_SITE';
